31e4218e5a9a8c0407f99d12dd5024ffd2c8e7d7
Widdy <wiiidddyyy@gmail.com>
2025-11-17 00:28:40 +0800

    Chaged Mico Skill Names, Added Sherwin

--- CharacterClass.java
+++ CharacterClass.java
@@ -7,12 +7,16 @@ public enum CharacterClass {
         new Skill("Charge", 25, 15, "Stun")
     )),
     MICO ("Mico", 80, 100, List.of(
-        new Skill("Fireball", 30, 20, "Burn"),
-        new Skill("Ice Shard", 20, 15, "Freeze")
+        new Skill("Ninjutsu", 30, 20, "Burn"),
+        new Skill("Domain Expansion", 20, 15, "Freeze")
     )),
     JENO ("Jeno", 100, 50, List.of(
         new Skill("Arrow Shot", 15, 5, "None"),
         new Skill("Poison Arrow", 10, 10, "Poison")
+    )),
+    SHERWIN("Sherwin", 100, 50, List.of(
+        new Skill("Rock Slam", 40, 15, "Stun"),
+        new Skill("Rock Solid", 15, 10, "Freeze")
     ));
 
     private final String displayName;
